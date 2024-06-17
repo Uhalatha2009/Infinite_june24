d96c0ed7689036b89dd5b8137de18e12dd306403
Uhalatha2009 <palepu.Uhalatha@infinite.com>
2024-06-17 12:13:59 +0530

    Add files via upload

---
+++ date 1/date 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace date_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"your name is {name} and you are {age} years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
